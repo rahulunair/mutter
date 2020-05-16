012e6d759375874ceed2a46aaa472739803de0de
rahulunair@gmail.com <rahulunair@gmail.com>
2020-05-16 01:46:58 -0700

    fuzzy license lookup added

--- Readme.md
+++ Readme.md
@@ -1,1 +1,16 @@
-## package version
+## Mutter
+
+Mutter is a tool i wrote because there was none to my liking and probably will not be useful to you.
+
+## What it does?
+
+well, the only thing it takes as arguments are 2 docker images, a `base image` and a `final image`, then it extracts the packages and versions
+installed in the final image along with the license(fuzzy and terribly in need to refactor this part). This can help when you want to ship an image
+and is too lazy to search for all the packages and extract their licenses manually. 
+
+
+## Which OS is supported?
+
+Supports any host that has a posix shell, but for now only debian based docker images are supported - debian, ubuntu, mint etc..
+
+
--- mutt.py
+++ mutt.py
@@ -1,5 +1,5 @@
-import csv
 import argparse
+import re
 import subprocess
 
 import pandas as pd
@@ -30,15 +30,21 @@ def get_pkgs_vers(image_name):
         print("exceptions..", e)
 
 
-def get_user_added_pkgs(base_image, final_image):
+def get_user_added_pkgs(base_image, final_image=None):
     """get unique packages that were installed in the final image"""
-    base_pkgs_vers, final_pkgs_vers = get_pkgs_vers(base_image), get_pkgs_vers(final_image)
+    if base_image and final_image:
+        base_pkgs_vers, final_pkgs_vers = get_pkgs_vers(base_image), get_pkgs_vers(final_image)
+    else:
+        final_pkgs_vers = base_pkgs_vers = get_pkgs_vers(base_image)
     base_pkgs, final_pkgs = set(), set()
     for bpkg in base_pkgs_vers:
         base_pkgs.add(bpkg.split(",")[0])
     for fpkg in final_pkgs_vers:
         final_pkgs.add(fpkg.split(",")[0])
-    uniq_pkgs = final_pkgs - base_pkgs
+    if final_pkgs_vers != base_pkgs_vers:
+        uniq_pkgs = final_pkgs - base_pkgs
+    else:
+        uniq_pkgs = base_pkgs
     uniq_pkgs_vers = set()
     for pkg in uniq_pkgs:
         for fpkg in final_pkgs_vers:
@@ -46,17 +52,41 @@ def get_user_added_pkgs(base_image, final_image):
                 uniq_pkgs_vers.add(fpkg)
     return sorted(uniq_pkgs_vers)
 
-def print_final_pkgs(uniq_pkgs):
+def get_pkg_license(image_name, pkg):
+    """a terrible fuzzy search of licenses"""
+    docker_run = f"docker run -t {image_name} bash -c "
+    get_pkg_license = ("'cat /usr/share/doc/{}/copyright | "
+        "grep -Ew -m1 "
+        "\"(BSD|GPL|MPL|Mozilla|"
+        "Apache|Creative|Artistic|"
+        "Boost|Public|License|MIT)\"'"
+    )
+    name = pkg["name"].split(":")[0]
+    license_file = get_pkg_license.format(name)
+    cat_license_cmd = docker_run + license_file
+    cat_license = (
+        subprocess.run(
+            cat_license_cmd, stdout=subprocess.PIPE, shell=True
+            ).stdout.decode("utf-8"))
+    return name, cat_license
+
+def print_final_pkgs(final_image, uniq_pkgs, file=None):
     """print pkgs one per line"""
     pkgs_vers = pd.DataFrame(columns=["name", "version", "license"])
     final_pkgs_vers = pd.DataFrame(uniq_pkgs, columns=["name_version"])
     pkgs_vers["name"], pkgs_vers["version"] = final_pkgs_vers["name_version"].str.split(",", 1).str
-    print(pkgs_vers)
-    #for pkg in uniq_pkgs:
-    #    print(pkg)
+    for _, pkg in pkgs_vers.iterrows():
+        _, license_text = get_pkg_license(final_image, pkg)
+        pkgs_vers.loc[pkgs_vers["name"]==pkg["name"], "license"] = license_text.rstrip()
+    pkgs_vers.to_csv(file)
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(pkgs_vers)
 
 
 if __name__ == "__main__":
     args = cf_parse()
     uniq_pkgs = get_user_added_pkgs(args.base_image, args.final_image)
-    print_final_pkgs(uniq_pkgs)
+    if args.final_image is None:
+        print_final_pkgs(args.base_image, uniq_pkgs, file="pkg_licenses.csv")
+    else:
+        print_final_pkgs(args.final_image, uniq_pkgs, file="pkg_licenses.csv")
